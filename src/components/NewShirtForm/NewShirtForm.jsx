28f8bcc1d08e14e087b56431cb3a4bea09769a9b
rafasanzvi <rafasv41@gmail.com>
2022-07-25 21:48:37 +0200

    Cloudinary added

--- src/components/NewShirtForm/NewShirtForm.jsx
+++ src/components/NewShirtForm/NewShirtForm.jsx
@@ -2,6 +2,7 @@ import { Form, Button, Row, Col } from "react-bootstrap"
 import { useState } from "react"
 import shirtsService from "../../services/shirt.services"
 import { useNavigate } from "react-router-dom"
+import uploadService from "../../services/upload.services"
 
 const NewShirtForm = () => {
 
@@ -32,7 +33,26 @@ const NewShirtForm = () => {
 
         shirtsService
             .saveShirt(shirtData)
-            .then(() => navigate('/list'))
+            .then(() => navigate('/shirts'))
+            .catch(err => console.error(err))
+
+    }
+
+    const [isLoading, setIsLoading] = useState(false)
+
+    const handleFileInput = e => {
+
+        setIsLoading(true)
+
+        const formData = new FormData()
+        formData.append('imageData', e.target.files[0])
+
+        uploadService
+            .uploadimage(formData)
+            .then(({ data }) => {
+                setIsLoading(false)
+                setShirtData({ ...shirtData, images: data.cloudinary_url })
+            })
             .catch(err => console.error(err))
 
     }
@@ -53,33 +73,33 @@ const NewShirtForm = () => {
 
         <Form onSubmit={handleSubmit}>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Group className="mb-3" controlId="name">
                 <Form.Label>Name</Form.Label>
                 <Form.Control type="text" value={name} onChange={handleChange} name="name" />
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Group className="mb-3" controlId="style">
                 <Form.Label>Style</Form.Label>
                 <Form.Control type="text" value={style} onChange={handleChange} name="style" />
             </Form.Group>
 
             <Row>
                 <Col>
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Group className="mb-3" controlId="colors">
                         <Form.Label>Colors</Form.Label>
                         <Form.Control type="text" value={colors} onChange={handleChange} name="colors" />
                     </Form.Group>
                 </Col>
 
                 <Col>
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Group className="mb-3" controlId="fabric">
                         <Form.Label>Fabric</Form.Label>
                         <Form.Control type="text" value={fabric} onChange={handleChange} name="fabric" />
                     </Form.Group>
                 </Col>
             </Row>
 
-            <Form.Select aria-label="Default select example" controlId="formBasicPassword" onChange={handleChange} name="origin" value={origin}>
+            <Form.Select aria-label="Default select example" controlId="origin" onChange={handleChange} name="origin" value={origin}>
                 <Form.Label>Origin</Form.Label>
                 <option value={""}>Select origin</option>
                 <option value={"Africa"}>Africa</option>
@@ -90,7 +110,7 @@ const NewShirtForm = () => {
             </Form.Select>
 
 
-            <Form.Select aria-label="Default select example" controlId="formBasicPassword" onChange={handleChange} name="sizes" value={sizes}>
+            <Form.Select aria-label="Default select example" controlId="sizes" onChange={handleChange} name="sizes" value={sizes}>
                 <Form.Label>Sizes</Form.Label>
                 <option value={""}>Select size</option>
                 <option value={"S"}>S</option>
@@ -99,19 +119,19 @@ const NewShirtForm = () => {
                 <option value={"XL"}>XL</option>
             </Form.Select>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Group className="mb-3" controlId="description">
                 <Form.Label>Description</Form.Label>
                 <Form.Control type="text" value={description} onChange={handleChange} name="description" />
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
+            {/* <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Image</Form.Label>
                 <Form.Control type="text" value={images} onChange={handleChange} name="images" />
-            </Form.Group>
+            </Form.Group> */}
 
-
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
+            <Form.Group className="mb-3" controlId="images">
+                <Form.Label>Image</Form.Label>
+                <Form.Control type="file" onChange={handleFileInput} name="images" />
             </Form.Group>
             <Button variant="secondary" type="submit">
                 Submit
