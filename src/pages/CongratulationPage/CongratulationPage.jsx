3ec8704ea590a6b934d02b967307cb1cf2d114e3
rafasanzvi <rafasv41@gmail.com>
2022-07-28 13:17:53 +0200

    Deploy done

---
+++ src/pages/CongratulationPage/CongratulationPage.jsx
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap"
+
+
+const Congratulation = () => {
+
+    return (
+        <Container>
+            <h1>Congratulations</h1>
+        </Container>
+    )
+}
